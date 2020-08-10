0470db9692cf7a59834359df77aa5440cb77f837
Kalpit Muley <kalpitmuley0@gmail.com>
2020-08-10 12:14:09 +0530

    Initial Commit

---
+++ Project1/src/main/java/study/assignment2/Project1/controller/BookResource.java
@@ -0,0 +1,71 @@
+package study.assignment2.Project1.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import study.assignment2.Project1.dto.BookDto;
+import study.assignment2.Project1.service.BookService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/library")
+public class BookResource {
+
+    Logger logger = LoggerFactory.getLogger(BookResource.class);
+
+    @Autowired
+    BookService bookService;
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping("/server/port")
+    public String getServerPort() {
+        logger.info("Server port API returned port: " + serverPort);
+        return "Server is running on port: " + serverPort;
+    }
+
+    @GetMapping("/book/all")
+    public List<BookDto> getAllBooks() {
+        List<BookDto> bookList = bookService.getAllBooks();
+        logger.info("List returned " + bookList.size() + " books");
+        return bookList;
+    }
+
+    @PostMapping("/book/add")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void addBook(@RequestBody BookDto bookDto) {
+        logger.info("Added book: " + bookDto);
+        bookService.addBook(bookDto);
+    }
+
+    @PostMapping("/book/addMultiple")
+    @ResponseStatus(HttpStatus.CREATED)
+    public String addMultipleBooks(@RequestBody List<BookDto> bookDtos) {
+        logger.info("Adding " + bookDtos.size() + " books");
+        bookService.addMultipleBooks(bookDtos);
+        return bookDtos.size() + " Books Added to the DB";
+    }
+
+    @PutMapping("/book/updateAuthor")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public String updateAuthorName(@RequestParam(value = "previousName", required = false, defaultValue = "Will") String previousName,
+                                 @RequestParam(value = "newName", required = false, defaultValue = "Williams") String updatedName) {
+        logger.info("Updating the Author name from" + previousName + " to " + updatedName);
+        Integer updatedRows = bookService.updateAuthorName(previousName, updatedName);
+        return updatedRows +" Rows updated";
+    }
+
+    @DeleteMapping("/book/delete")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public String deleteBooksCostlierThan(@RequestParam(value = "cost", required = false, defaultValue = "1000") Integer cost) {
+        logger.info("Deleting books costlier than: " + cost);
+        Integer deletedRows = bookService.deleteBooksCostlierThan(cost);
+        return deletedRows +" entries deleted";
+    }
+
+}
---
+++ Project1/src/main/java/study/assignment2/Project1/model/BookEntity.java
@@ -0,0 +1,88 @@
+package study.assignment2.Project1.model;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity(name = "Book")
+public class BookEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(updatable = false, nullable = false)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column(name = "author_Name")
+    private String authorName;
+
+    @Column
+    private Integer cost;
+
+    @CreationTimestamp
+    private Date timestamp;
+
+    @UpdateTimestamp
+    private Date lastsavedate;
+
+    public BookEntity() {
+    }
+
+    public BookEntity(String name, String authorName, Integer cost) {
+        this.name = name;
+        this.authorName = authorName;
+        this.cost = cost;
+    }
+
+    public Date getLastsavedate() {
+        return lastsavedate;
+    }
+
+    public void setLastsavedate(Date lastsavedate) {
+        this.lastsavedate = lastsavedate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public Integer getCost() {
+        return cost;
+    }
+
+    public void setCost(Integer cost) {
+        this.cost = cost;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+}
